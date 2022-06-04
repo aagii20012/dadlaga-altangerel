ef53f6fe2a8660cf9f27368d51dd48fd8070be7b
altangerel <aagii200148@gmail.com>
2022-06-04 18:50:22 +0800

    added problem 20

--- src/problem/Problem20.java
+++ src/problem/Problem20.java
@@ -1,12 +1,26 @@
 package problem;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.GroupLayout;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.GroupLayout.Alignment;
+
+import main.main;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import javax.swing.JTextField;
+import javax.swing.JFormattedTextField;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class Problem20 {
 
 	private JFrame frame;
+	private JTextField alphabetField;
 
 	/**
 	 * Launch the application.
@@ -38,6 +52,104 @@ public class Problem20 {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JButton btnNewButton = new JButton("back");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+				main mainPage=new main();
+				mainPage.main(null);
+			}
+		});
+		
+		String quest="Англи цагаан толгой үсгийг харгалзах жижиг эсвэл том үсгийн хамтаар хэвлэ";
+		
+		JLabel lblQuestLabel = new JLabel("<html>"+quest+"</html>");
+		
+		JLabel lblNewLabel = new JLabel("Enter alphabet");
+		
+		JLabel lblResultLabel = new JLabel("Result");
+		
+		alphabetField = new JTextField();
+		alphabetField.addKeyListener(new KeyAdapter() {
+			@Override
+			public void keyTyped(KeyEvent e) {
+				if (alphabetField.getText().length() >= 1 ) // limit to 3 characters
+					alphabetField.setText(alphabetField.getText().substring(0, 0));
+			}
+		});
+		alphabetField.setColumns(10);
+		
+		JButton btnShowButton = new JButton("Show");
+		btnShowButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String alphabet = null;
+				
+				alphabet = alphabetField.getText();
+				if(alphabet.isBlank())
+				{
+					lblResultLabel.setText("Error: Please enter alphabet");
+					return;
+				}
+				if(isNumeric(alphabet))
+				{
+					lblResultLabel.setText("Error: It must be alphabet");
+					return;
+				}
+				
+				lblResultLabel.setText("Result: " + alphabet.toLowerCase() + " " + alphabet.toUpperCase());
+			}
+		});
+		
+		GroupLayout groupLayout = new GroupLayout(frame.getContentPane());
+		groupLayout.setHorizontalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
+							.addContainerGap()
+							.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 72, GroupLayout.PREFERRED_SIZE))
+						.addGroup(groupLayout.createSequentialGroup()
+							.addGap(63)
+							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+								.addComponent(lblQuestLabel)
+								.addGroup(groupLayout.createSequentialGroup()
+									.addComponent(lblNewLabel)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(alphabetField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+									.addPreferredGap(ComponentPlacement.UNRELATED)
+									.addComponent(btnShowButton))
+								.addGroup(groupLayout.createSequentialGroup()
+									.addGap(10)
+									.addComponent(lblResultLabel)))))
+					.addContainerGap(14, Short.MAX_VALUE))
+		);
+		groupLayout.setVerticalGroup(
+			groupLayout.createParallelGroup(Alignment.LEADING)
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(btnNewButton)
+					.addGap(18)
+					.addComponent(lblQuestLabel)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblNewLabel)
+						.addComponent(alphabetField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(btnShowButton))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(lblResultLabel)
+					.addGap(132))
+		);
+		frame.getContentPane().setLayout(groupLayout);
+	}
+	
+	public static boolean isNumeric(String str) { 
+		  try {  
+		    Double.parseDouble(str);  
+		    return true;
+		  } catch(NumberFormatException e){  
+		    return false;  
+		  }  
 	}
 
 }
